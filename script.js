e55f57bf8863269f23f5a196726673432f1d7762
aianxabi0-crypto <aianxabi0@gmail.com>
2026-02-15 22:41:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('getGamesBtn');
+
+button.addEventListener('click', function() {
+    // Сценарий 1: Перенаправляем на страницу с формой
+    window.location.href = 'redirect.html';
+});
